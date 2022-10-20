c3085c273a427bc7067d7cbdd17896a17e1918c6
Victor Macabu <victor.macabu12@gmail.com>
2022-10-20 16:39:06 -0300

    construções de funcionalidade painel de dados

---
+++ JS/painel_dados/painel_seges_digital.js
@@ -0,0 +1,7 @@
+const iframes_titulos = ['Fisico','SEGES DIGITAL', 'SEGES DIGITAL Atualizado'];
+const iframes_links = [
+"https://app.powerbi.com/reportEmbed?reportId=39948c4b-ae11-4c27-a86b-572773c98697&autoAuth=true&ctid=f398df9c-fd0c-4829-a003-c770a1c4a063",
+"https://app.powerbi.com/reportEmbed?reportId=e00dad84-eba8-4e72-9af9-f15e194452de&autoAuth=true&ctid=f398df9c-fd0c-4829-a003-c770a1c4a063",
+"https://app.powerbi.com/reportEmbed?reportId=f9997d95-f14d-4dd5-bb0a-dc0292c44dc5&autoAuth=true&ctid=f398df9c-fd0c-4829-a003-c770a1c4a063"
+]
+
