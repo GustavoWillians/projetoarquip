b5886f8dc8d57a8761aa93f1e51db324353d7a5e
victorMacabu <victor.macabu12@gmail.com>
2022-10-24 10:28:33 -0300

    add função troca para painel de dados

--- JS/painel_dados/painel_seges_digital.js
+++ JS/painel_dados/painel_seges_digital.js
@@ -1,17 +1,39 @@
-const iframes_titulos = ['Fisico','SEGES DIGITAL', 'SEGES DIGITAL Atualizado'];
+const iframes = [
+  {
+    titulo: 'Fisico',
+    link: 'https://app.powerbi.com/reportEmbed?reportId=39948c4b-ae11-4c27-a86b-572773c98697&autoAuth=true&ctid=f398df9c-fd0c-4829-a003-c770a1c4a063'
+  },{
+    titulo:'SEGES DIGITAL',
+    link:'https://app.powerbi.com/reportEmbed?reportId=e00dad84-eba8-4e72-9af9-f15e194452de&autoAuth=true&ctid=f398df9c-fd0c-4829-a003-c770a1c4a063'
+  },{
+    titulo: 'SEI',
+    link:'https://sip.prefeitura.sp.gov.br'
+  },{
+    titulo: 'Gestão Documental',
+    link:'http://gestaodocumental.prefeitura.sp.gov.br/'
+  },{
+    titulo: 'Diário Oficial',
+    link:'http://gestaodocumental.prefeitura.sp.gov.br/diario-2-0/'
+  },{
+    titulo: 'Portal de Processos',
+    link:'https://processos.prefeitura.sp.gov.br/Forms/Principal.aspx'
+  }];
 
-const iframes_links = [
-"https://app.powerbi.com/reportEmbed?reportId=39948c4b-ae11-4c27-a86b-572773c98697&autoAuth=true&ctid=f398df9c-fd0c-4829-a003-c770a1c4a063",
-"https://app.powerbi.com/reportEmbed?reportId=e00dad84-eba8-4e72-9af9-f15e194452de&autoAuth=true&ctid=f398df9c-fd0c-4829-a003-c770a1c4a063",
-"https://app.powerbi.com/reportEmbed?reportId=f9997d95-f14d-4dd5-bb0a-dc0292c44dc5&autoAuth=true&ctid=f398df9c-fd0c-4829-a003-c770a1c4a063"
-]
+
 let botoes = document.getElementsByClassName('controls-iframe');
 let iframe_atual = document.getElementById('iframe_dados');
 
-for(let i = 0; i < iframes_titulos.length; i++){
+for(let i = 0; i < iframes.length; i++){
 
-  botoes[0].innerHTML +=  '<div class="botoes"><button>'+ iframes_titulos[i] + '</button></div>';
+  botoes[0].innerHTML +=  '<div class="botoes"><button>'+ iframes[i].titulo + '</button></div>';
 }
-function troca(){
-  
+
+let btn_iframe = document.getElementsByClassName('botoes');
+
+function troca(troca){
+  iframe_atual.src = troca;
+}
+
+for(let i = 0; i < btn_iframe.length; i++){
+  btn_iframe[i].onclick = () => {troca(iframes[i].link)};
 }
