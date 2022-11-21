77307d0480c0b60fc0a52c38899ce322a69e1851
victorMacabu <victor.macabu12@gmail.com>
2022-11-21 15:25:19 -0300

    menu iframe

--- JS/painel_dados/painel_seges_digital.js
+++ JS/painel_dados/painel_seges_digital.js
@@ -1,8 +1,21 @@
 const iframes = [
   {
+    titulo: 'TESTE',
+    link:'https://app.powerbi.com/reportEmbed?reportId=849fd5bb-a9d3-42f1-9e94-27cb507d3e57&autoAuth=true&ctid=f398df9c-fd0c-4829-a003-c770a1c4a063'
+  },
+  {
+    titulo: 'TESTE',
+    link:'https://app.powerbi.com/reportEmbed?reportId=849fd5bb-a9d3-42f1-9e94-27cb507d3e57&autoAuth=true&ctid=f398df9c-fd0c-4829-a003-c770a1c4a063'
+  },
+  {
+    titulo: 'TESTE',
+    link:'https://app.powerbi.com/reportEmbed?reportId=849fd5bb-a9d3-42f1-9e94-27cb507d3e57&autoAuth=true&ctid=f398df9c-fd0c-4829-a003-c770a1c4a063'
+  },
+  {
     titulo: 'SEI',
     link:'https://app.powerbi.com/reportEmbed?reportId=849fd5bb-a9d3-42f1-9e94-27cb507d3e57&autoAuth=true&ctid=f398df9c-fd0c-4829-a003-c770a1c4a063'
-  },{
+  },
+  {
     titulo: 'SIMPROC',
     link:'https://app.powerbi.com/reportEmbed?reportId=bbc9f652-da28-42db-9823-454b859734a8&autoAuth=true&ctid=f398df9c-fd0c-4829-a003-c770a1c4a063'
   }];
@@ -26,11 +39,17 @@ for(let i = 0; i < btn_iframe.length; i++){
   let select;
   btn_iframe[i].onclick = () => {
     troca(iframes[i].link)
-    btn_iframe[i].style.backgroundColor = "#0b2768";
     select = i;
+    btn_iframe[select].style.border = "none";
+    btn_iframe[i].style.backgroundColor = "#fff";
+    btn_iframe[i].style.borderLeft = "2px solid #000";
+    btn_iframe[i].style.borderTop = "2px solid #000";
+    btn_iframe[i].style.borderRight = "2px solid #000";
     for(let i = 0; i < btn_iframe.length; i++){
       if (i != select){
-        btn_iframe[i].style.backgroundColor = "#fa8d5b";
+        btn_iframe[i].style.border = "none";
+        btn_iframe[i].style.backgroundColor = "#289ddb";
+        btn_iframe[i].style.borderBottom = "2px solid #000";
       }
     }
   }
