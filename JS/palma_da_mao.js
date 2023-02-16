c9c61996c1da33be1842a746d035eb26870179ce
victorMacabu <victor.macabu12@gmail.com>
2023-02-16 10:26:29 -0300

    edição dos ícones e links arquip na palma da mão

--- JS/palma_da_mao.js
+++ JS/palma_da_mao.js
@@ -1,16 +1,8 @@
 const apps = [
   {
-    img:'cac.png',
-    nome: 'CAC',
-    link: 'https://admin.cacplus.prodam/Autenticacao?ReturnUrl=%2f'
-  },{
     img:'DIARIO-OFICIAL-NOVO.png',
     nome: 'Diário Oficial NOVO',
     link: 'https://epubli.prefeitura.sp.gov.br/md_epubli_controlador.php?acao=inicio'
-  },{
-    img:'diario2.png',
-    nome: 'Diário Oficial',
-    link: 'http://www.docidadesp.imprensaoficial.com.br/Busca.aspx'
   },{
     img:'PORTAL-PROCESSOS.png',
     nome: 'Portal de Processos ',
@@ -20,25 +12,41 @@ const apps = [
     nome: 'SEI',
     link: 'https://sip.prefeitura.sp.gov.br/sip/login.php?sigla_orgao_sistema=PMSP&sigla_sistema=SEI&infra_url=L3NlaS8='
   },{
-    img:'SEI-TREINAMENTO.png',
-    nome: 'SEI TREINAMENTO',
-    link: 'https://treinamentosip4.prefeitura.sp.gov.br/sip/login.php?sigla_orgao_sistema=PMSP&sigla_sistema=SEI&infra_url=L3NlaS8='
+    img:'Dados.png',
+    nome: 'DADOS',
+    link: 'https://arquip.prefeitura.sp.gov.br/app/painel_dados/'
   },{
-    img:'simproc-web.png',
-    nome: 'Simproc Web',
-    link: 'https://simproc.prefeitura.sp.gov.br/Forms/login.aspx'
+    img:'PGDOC.png',
+    nome: 'PGDOC',
+    link: 'https://arquip.prefeitura.sp.gov.br/app/pgdoc/'
   },{
-    img:'simproc.png',
-    nome: 'Simproc (Ambiente Anterior)',
-    link: 'https://simprocantigo.prefeitura.sp.gov.br/SJ0934/servlet/LOGIN'
+    img:'SAMSP.png',
+    nome: 'SAMSP',
+    link: 'https://www.prefeitura.sp.gov.br/cidade/secretarias/gestao/arquivo_publico/institucional/index.php'
   },{
-    img:'sip.png',
-    nome: 'SIP',
-    link: 'https://sip.prefeitura.sp.gov.br/sip/login.php?sigla_orgao_sistema=PMSP&sigla_sistema=SIP&infra_url=L3NpcC8='
+    img:'sistemas.png',
+    nome: 'Sistemas',
+    link: 'https://arquip.prefeitura.sp.gov.br/sistemas.html'
   },{
-    img:'TID.png',
-    nome: 'TID',
-    link: 'http://tid.prodam.sp.gov.br'
+    img:'infograficos.png',
+    nome: 'Infográficos',
+    link: ''
+  },{
+    img:'Suporte.png',
+    nome: 'Suporte',
+    link: 'https://arquip.prefeitura.sp.gov.br/chat/'
+  },{
+    img:'----.png',
+    nome: '------',
+    link: ''
+  },{
+    img:'----.png',
+    nome: '------',
+    link: ''
+  },{
+    img:'----.png',
+    nome: '------',
+    link: ''
   },
 ];
     
@@ -49,8 +57,8 @@ for(let i = 0; i < apps.length; i++){
   container[0].innerHTML +=  `
         <div class="sistemApps">
           <a target="_blank" href="${apps[i].link}">
-            <img src="./Assets/arquip-na-mao/${apps[i].img}" alt="link sistema ${apps[i].nome}">
-            <p>${apps[i].nome}</p>
+          <img src="./Assets/arquip-na-mao/${apps[i].img}" alt="link sistema ${apps[i].nome}"> 
+          <p>${apps[i].nome}</p>
           </a>
         </div>`;
 }
